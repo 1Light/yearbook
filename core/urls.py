ce10e0c4e31d65f3de01e8c3ab4d2ebb0edf8273
1Light <redclover1.618@gmail.com>
2025-07-09 19:29:25 +0300

    upload

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import LoginAPIView, ListUsersView, encoders_by_campus, students_by_encoder, check_storage
 
 urlpatterns = [
-    path('login/', LoginAPIView.as_view(), name='login'),
+    path('login', LoginAPIView.as_view(), name='login'),
     path("debug-users/", ListUsersView.as_view()),
     path('encoders/campus/<str:university_name>/', encoders_by_campus, name='encoders_by_campus'),
     path('debug/check-storage/', check_storage, name='check_storage'),
