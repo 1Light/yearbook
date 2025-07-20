96c7b363c36f3e1a7a04ac6070aa15b70a9949fb
1Light <redclover1.618@gmail.com>
2025-07-20 13:14:55 +0300

    upload

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import RSVPToken
+
+@admin.register(RSVPToken)
+class RSVPTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'token', 'responded', 'response')
+    search_fields = ('user__email',)
+    list_filter = ('responded', 'response')
